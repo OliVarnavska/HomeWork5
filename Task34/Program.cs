220dcbdcee7978cad3fb408dcc336485c0e85cd7
Oli <oli.varnavska@gmail.com>
2023-03-24 22:01:38 +0800

    Fix Task 34

---
+++ Task34/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 34: Задайте массив заполненный 
+// случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество
+//  чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int Prompt(string msg)
+{
+
+    Console.Write(msg); //Выводим приглашение ко вводу
+    string ReadInput = Console.ReadLine();
+    int result = int.Parse(ReadInput); //приводим к числу
+    return result; //возвращаем результат
+
+}
+//метод для получения случайных значений массива
+int[] Array(int Length)
+{
+
+    int[] array = new int[Length];//объявляем массив 
+    Random random = new Random();
+    for (int i = 0; i < Length; i++)
+    {
+        array[i] = random.Next(100, 999); //заполняем случайными
+        //цифрами из диапазона SatrArr до EndArr
+
+    }
+
+    return array;
+}
+// int count = 0;
+// if ( array[i] % 2 == 0) count = count+1;
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+        }
+    Console.Write($"{array[array.Length - 1]}");
+    Console.WriteLine("]");
+
+}
+
+int length = Prompt("Длинна массива: ");
+int[] array = Array(length);
+PrintArray(array);
+int [] array1 = array;
+int x = 0;
+for (int j = 0; j < length; j++)
+{
+ if (array[j] % 2 == 0) 
+ {
+    x=x+1;
+ }  
+}
+Console.WriteLine(x);
+
+
